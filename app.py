0095df0841f1672af12cfe930d649663929f8c22
kyleoconnor-dev <koconno5190@gmail.com>
2022-12-15 04:43:33 +0000

    updated app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/get_vwtds_data', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_vwtds():
     results = vwtds.run_vwtds()
     return results
